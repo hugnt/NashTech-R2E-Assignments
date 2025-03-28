d75752ffad9fb43d93e2d6d96c39d03434ade50f
hugnt <thanhhungst314@gmail.com>
2025-03-28 18:07:35 +0700

    add serilog

--- Assignment03/Assignment03.API/Program.cs
+++ Assignment03/Assignment03.API/Program.cs
@@ -19,6 +19,7 @@ namespace Assignment03.API
 
 
             //add DI service
+            //builder.Services.AddSingleton<ILoggingService, SeriLogService>();
             builder.Services.AddSingleton<ILoggingService, LoggingService>();
             builder.Services.AddSingleton<IStudentService, StudentService>();
 
---
+++ Assignment03/Assignment03.API/Services/SeriLogService.cs
@@ -0,0 +1,45 @@
+﻿using Assignment03.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace Assignment03.API.Services;
+
+public class SeriLogService : ILoggingService
+{
+    private readonly string _logRequestFilePath;
+    private readonly string _logResponseFilePath;
+    public SeriLogService(IWebHostEnvironment env)
+    {
+         Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File("Logging/serilog.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+    }
+    public async void LogRequestInformation(LoggingRequestModel loggingRequestModel)
+    {
+       
+
+        StringBuilder logMessage = new StringBuilder();
+        logMessage.AppendLine($"Timestamp: {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+        logMessage.AppendLine($"Scheme: {loggingRequestModel.Schema}");
+        logMessage.AppendLine($"Host: {loggingRequestModel.Host.Value}");
+        logMessage.AppendLine($"Path: {loggingRequestModel.Path}");
+        logMessage.AppendLine($"QueryString: {loggingRequestModel.QueryString}");
+        logMessage.AppendLine($"Request Body: {loggingRequestModel.RequestBody}");
+        logMessage.AppendLine("-----------------------------------------");
+
+
+        Log.Information(logMessage.ToString());
+    }
+
+    public async void LogResponseInformation(LoggingResponseModel loggingResponseModel)
+    {
+        Log.Information(loggingResponseModel.ResponseBody);
+    }
+
+    public async void LogMessage(string message) => Log.Error(message);
+}
