020e7f781114710a516b0e3a8c5f31f9c98769c6
HUG <thanhhungst314@gmail.com>
2025-04-08 23:08:04 +0700

    fix double folder git

---
+++ Assignment07-API/Assignment07.API/Middlewares/ResultHandlingMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using Assignment07.Domain.Shared;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Assignment07.API.Middlewares;
+
+public class ResultHandlingMiddleware
+{
+	private readonly RequestDelegate _next;
+
+	public ResultHandlingMiddleware(RequestDelegate next)
+	{
+		_next = next;
+	}
+
+	public async Task InvokeAsync(HttpContext httpContext)
+	{
+		// Bỏ qua các request đến Swagger
+		if (httpContext.Request.Path.StartsWithSegments("/swagger"))
+		{
+			await _next(httpContext);
+			return;
+		}
+		var originalBodyStream = httpContext.Response.Body;
+
+		try
+		{
+			// Tạo một memory stream để tạm thời chứa response
+			using var responseBody = new MemoryStream();
+			httpContext.Response.Body = responseBody;
+
+			await _next(httpContext);
+
+			responseBody.Seek(0, SeekOrigin.Begin);
+			var responseText = await new StreamReader(responseBody).ReadToEndAsync();
+
+			responseBody.Seek(0, SeekOrigin.Begin);
+
+			try
+			{
+				var result = JsonConvert.DeserializeObject<Result>(responseText);
+				if (result != null)
+				{
+					httpContext.Response.StatusCode = (int)result.StatusCode;
+				}
+			}
+			catch (JsonException)
+			{
+				// Nếu không phải Result object thì giữ nguyên status code mặc định
+			}
+
+			await responseBody.CopyToAsync(originalBodyStream);
+		}
+		finally
+		{
+			httpContext.Response.Body = originalBodyStream;
+		}
+	}
+}
